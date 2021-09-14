be715944122a3be8b69be83174f5b668d371f3be
jstrickler <jstrickler@anybotics.com>
2021-09-14 10:15:42 +0200

    Add pause visualization to evaluation graph

--- Logs/evaluate.py
+++ Logs/evaluate.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-LOG_FILE = 'plant_20210913_1306.log'
+LOG_FILE = 'plant_20210914_1007.log'
 
 with open(LOG_FILE, 'r') as f:
     tankA_level = []
@@ -12,6 +12,7 @@ with open(LOG_FILE, 'r') as f:
 
     user_time = []
     event_time = []
+    pause_time = []
 
     tolerance = 0
     participant = 0
@@ -33,11 +34,17 @@ with open(LOG_FILE, 'r') as f:
             tankB_time.append(datetime.strptime(split_string[0], "%H:%M:%S"))
             tankB_level.append(split_string[-1][:-1])
 
-        if 'INPUT' in split_string:
+        if 'INPUT' in split_string and 'MAIN' in split_string:
             user_time.append(datetime.strptime(split_string[0], "%H:%M:%S"))
 
         if 'FAIL\n' in split_string:
             event_time.append(datetime.strptime(split_string[0], "%H:%M:%S"))
+
+        if 'PAUSE\n' in split_string:
+            pause_time.append(datetime.strptime(split_string[0], "%H:%M:%S"))
+
+        if 'RESUME\n' in split_string:
+            pause_time.append(datetime.strptime(split_string[0], "%H:%M:%S"))
 
 combined = [(abs(int(a) - 2500) + abs(int(b) - 2500)) // 2 for a, b in zip(tankA_level, tankB_level)]
 plt.plot(tankA_time, combined, label='Combined Tanks')
@@ -45,6 +52,8 @@ for xc in user_time:
     plt.axvline(x=xc, color='g', linestyle='dotted', label='User Input')
 for xc in event_time:
     plt.axvline(x=xc, color='r', linestyle='dashdot', label='Tank Failure')
+for xi in range(0, len(pause_time), 2):
+    plt.axvspan(pause_time[xi], pause_time[xi+1], color=(0.1, 0.2, 0.5, 0.3), linestyle='solid', label='Pause')
 plt.axhline(y=tolerance, color='b', linestyle='dashed', label='Critical Tolerance')
 # Add title and axis names
 plt.title('Participant ' + participant + ': ' + LOG_FILE)
@@ -53,4 +62,5 @@ plt.ylabel('Tank Level Δ')
 handles, labels = plt.gca().get_legend_handles_labels()
 by_label = dict(zip(labels, handles))
 plt.legend(by_label.values(), by_label.keys(), loc=1)
+plt.xlim([tankA_time[0], tankA_time[-1]])
 plt.show()
